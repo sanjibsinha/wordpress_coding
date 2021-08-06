025e7a37b8d7329f732c7ed9581313a424309820
sanjibsinha <sanjib12sinha@gmail.com>
2021-08-06 06:05:45 +0530

    fourth commit

--- content.php
+++ content.php
@@ -1,31 +1,59 @@
-<?php
-
-if(have_posts(  )): while(have_posts(  )): the_post(  ); ?>
-
-<h1>
-        <?php the_title(); ?>
-</h1>
-<p>
-          
-          Posted by 
-    <?php the_author_posts_link(); ?>
-    on
-    <?php the_time( 'd.m.y' ) ?>
-    in
-    <?php echo get_the_category_list(', '); ?>
-</p>
-<p class="container">
-	<?php
-if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-the_post_thumbnail( 'full' );
-}
-?>
-</p>	
-<article class="blog-post">
-      <?php the_content( ); ?>
-</article> 
-
-
-<?php
-endwhile; else: endif;
-?>
+<div class="container py-5">
+     <!-- First Row [Statistics]-->
+     <h2 class="font-weight-bold mb-2">Latest Statistics</h2>
+    <p class="font-italic text-muted mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+    
+    <div class="row pb-5">
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Card-->
+            <div class="card rounded shadow-sm border-0">
+                <div class="card-body p-5"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
+                    <h5>Products Sales</h5>
+                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div class="progress rounded-pill">
+                        <div role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;" class="progress-bar rounded-pill"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Card -->
+            <div class="card rounded shadow-sm border-0">
+                <div class="card-body p-5"><i class="fa fa-tasks fa-2x mb-3 text-success"></i>
+                    <h5>Completed Tasks</h5>
+                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div class="progress rounded-pill">
+                        <div role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;" class="progress-bar bg-success rounded-pill"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <!-- Card -->
+            <div class="card rounded shadow-sm border-0">
+                <div class="card-body p-5"><i class="fa fa-user-circle-o fa-2x mb-3 text-info"></i>
+                    <h5>New Users</h5>
+                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div class="progress rounded-pill">
+                        <div role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;" class="progress-bar bg-info rounded-pill"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Card -->
+        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
+            <div class="card rounded shadow-sm border-0">
+                <div class="card-body p-5"><i class="fa fa-shopping-bag fa-2x mb-3 text-warning"></i>
+                    <h5>New Products</h5>
+                    <p class="small text-muted font-italic">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+                    <div class="progress rounded-pill">
+                        <div role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;" class="progress-bar bg-warning rounded-pill"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- header.php
+++ header.php
@@ -9,8 +9,40 @@
     />
 </head>
 <body>
+    <!-- NAVBAR-->
+<nav class="navbar navbar-expand-lg py-3 navbar-dark bg-dark shadow-sm">
+  <div class="container">
+    <a href="#" class="navbar-brand">
+      <!-- Logo Image -->
+      <img src="https://res.cloudinary.com/mhmd/image/upload/v1557368579/logo_iqjuay.png" width="45" alt="" class="d-inline-block align-middle mr-2">
+      <!-- Logo Text -->
+      <span class="text-uppercase font-weight-bold">Company</span>
+    </a>
+
+    <button type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+
+    <div id="navbarSupportedContent" class="collapse navbar-collapse">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active"><a href="#" class="nav-link">Home <span class="sr-only">(current)</span></a></li>
+        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Services</a></li>
+        <li class="nav-item"><a href="#" class="nav-link">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
 <div id="container">
-    <div id="body">
-    <h2>
-    <a href="/">HOME</a>
-    </h2>
+<div style="background: url(https://i.postimg.cc/3N7wnb75/background.jpg)" class="jumbotron bg-cover text-white">
+    <div class="container py-5 text-center">
+        <h1 class="display-4 font-weight-bold">Bootstrap static header</h1>
+        <p class="font-italic mb-0">Using simple jumbotron-style component, create a nice Bootstrap 4 header with a background image.</p>
+        <p class="font-italic">Snippe by
+            <a href="https://bootstrapious.com" class="text-white">
+                <u>Bootstrapious</u>
+            </a>
+        </p>
+        <a href="#" role="button" class="btn btn-primary px-5">See All Features</a>
+    </div>
+</div>
+
+
